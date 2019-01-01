97f2e19318b51a003693d374e786064d71b1b0b3
zhlsuo <zhlsuo@163.com>
2019-01-01 16:38:09 +0800

    bug

--- DPTBD_ZHLSUO.m
+++ DPTBD_ZHLSUO.m
@@ -23,31 +23,19 @@ SNR=20;    % 什么用处？
 Nx = 50;   %径向距离分辨单元    
 Ny = 50;   %切向距离分辨单元
 DataScan = DP_GenerateData(x, SNR, Nx, Ny, F_Cnt, Theta , Power_noise_av);
+%%
 
 %%数据处理
 DataScan_Processed = DP_MainAlgorithm(DataScan);
-
+%%
+%%获取轨迹
 VT = 150; %门限
 TargetTrace = DP_FindTargetTrack(DataScan_Processed,VT);
-
+%%
+%%过滤重复的轨迹
 RealTrace = DPTBD_FindTrace(TargetTrace,DataScan_Processed);
+%%
 
-n=ndims(RealTrace);
-s=size(RealTrace);
-if n == 2
-    loop = 1;
-else
-    loop = s(3);
-end
-loop
-
-coArray=['r','g','c','b','y'];%初始颜色数组
-liArray=['o','x','+','*','^'];%初始线条数组
-
-for i =1:loop
-    liVal=liArray(1 + rem(i,length(coArray)));%取得随机线条
-    coVal=coArray(1 + rem(i,length(coArray)));%取得随机颜色
-    plot(RealTrace(:,1,i),RealTrace(:,2,i),[coVal '-' liVal]);
-    hold on
-end
-
+%%打印轨迹
+DPTBD_ShowTrace(RealTrace);
+%%
